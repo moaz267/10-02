462af1f36243e14aa35482e9699ec2787ec178bb
MAOZ <161624227+moaz267@users.noreply.github.com>
2024-10-09 03:35:58 +0300

    2-10

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <h1>welcome to Home page</h1>
+      <p>notice : add to cart button is working </p>
+       </div>
+  )
+}
